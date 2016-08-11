28c586378758ee9e2e6a6b9629a19b6c516ef983
felix0the0cat <josh.borovoy@gmail.com>
2016-08-10 22:02:25 -0400

    Add files via upload

--- code_challenge.py
+++ code_challenge.py
@@ -1,11 +1,15 @@
-import json
-import re
+## Sortable Coding Challenge
+# Written by: Josh Borovoy
+# Completed on: August 9th, 2016
+## Take a list of products with information and compare it to a set of listings potentially related to each product in the list and match them up
+
+import json #needed for the JSON manipulation
 
 def record_json_data(filename):
-    data = []
-    with open(filename) as data_file:
-        for line in data_file:
-            data.append(json.loads(line))
+    data = [] #initialize array that will become the data from each file
+    with open(filename) as data_file: # open the file
+        for line in data_file: # go through each line of the input file
+            data.append(json.loads(line)) #load the JSON file in
     return data
 
 if __name__ == '__main__':
@@ -16,35 +20,35 @@ if __name__ == '__main__':
 
     for line in products:
         value = {"product_name":line['product_name'],
-                "listings":[]
+                "listings":[] #create product name with listings dictionary list
                 }
-        matched_list.append(value)
+        matched_list.append(value) # add the line to the product name
 
 
     for line in listings:
-        a=line['title']
-        test = 0
+        a=line['title'] #get the title for each listings item
+        count = 0 # initialize the number to co-incide with the matched_list
         for lines in products:
-            if line['manufacturer'] == lines['manufacturer']:
-                if a.find(' '+lines['model']+' ') > -1:
+            if line['manufacturer'] == lines['manufacturer']: #check manufacturer first because the data is in both files. Easiest check
+                if a.find(' '+lines['model']+' ') > -1: # check for the name of the model in the title
                     if 'family' in lines: #double check with family, if possible
-                        if a.find(lines['family']+' ') > -1:
-                            matched_list[test]['listings'].append(line)
-                            test +=1
+                        if a.find(lines['family']+' ') > -1: # when checking for the model, to ensure that it's not part of another product's name look for a space before and after
+                            matched_list[count]['listings'].append(line) # add the listing value to the appropraite product_name, array set up before
+                            count +=1
                             break
                         else:
-                            test +=1
+                            count +=1
                             continue
                     else: # if no family, it still matches so far
-                        matched_list[test]['listings'].append(line)
-                        test +=1
+                        matched_list[count]['listings'].append(line)
+                        count +=1
                         break
                 else:
-                    test +=1
+                    count +=1
             else:
-                test +=1
+                count +=1
 
-    output = open('results.txt','w')
-    for item in matched_list:
-        print>>output, json.dumps(item)
-    output.close()
+    output = open('results.txt','w') # open a file to write the item to
+    for item in matched_list: # go through each item in the array containing the matched items
+        print>>output, json.dumps(item) # print each dictionary in the array line by line
+    output.close() # close the file, end the program
